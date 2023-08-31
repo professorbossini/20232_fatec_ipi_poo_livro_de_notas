8a2fb653e0fd750f84c9b748c010480a30ea9d3a
Rodrigo Bossini <professorbossini@gmail.com>
2023-08-31 18:04:45 -0300

    ap06(java): teste do livro de notas

---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,9 @@
+public class TesteLivroDeNotas{
+  static public void main(String args []){
+    //variável de referência
+    //malloc
+    LivroDeNotas livroDeNotas = new LivroDeNotas();
+    livroDeNotas.exibirMensagem();
+    System.out.println("Até logo");
+  }
+}
